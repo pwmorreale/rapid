1b176814b3c4cacd35dfeb9d097bb4db1ee83ba2
Peter W. Morreale <pwmorreale@gmail.com>
2025-04-27 19:46:18 -0600

    Complete verify command.
    Implement the verify command.
    
    Signed-off-by: Peter W. Morreale <pwmorreale@gmail.com>

--- cmd/verify.go
+++ cmd/verify.go
@@ -6,8 +6,10 @@
 package cmd
 
 import (
-	"fmt"
+	"os"
 
+	"github.com/pwmorreale/rapid/internal/logger"
+	"github.com/pwmorreale/rapid/internal/verify"
 	"github.com/spf13/cobra"
 )
 
@@ -16,11 +18,36 @@ var verifyCmd = &cobra.Command{
 	Use:   "verify",
 	Short: "Verify a scenario configuration",
 	Long:  `Verify syntax and elements of a yaml senario configuration`,
-	Run: func(_ *cobra.Command, _ []string) {
-		fmt.Println("verify called", scenarioFile)
-	},
+	RunE:  DoVerify,
 }
 
 func init() {
 	rootCmd.AddCommand(verifyCmd)
+}
+
+// StartVerify starts the verify command.
+func DoVerify(_ *cobra.Command, _ []string) error {
+
+	opts := logger.Options{
+		Writer:    os.Stdout,
+		Handler:   logFormat,
+		Level:     logLevel,
+		Timestamp: logTimestamp,
+	}
+
+	if logFilename != "" {
+		file, err := os.OpenFile(logFilename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		opts.Writer = file
+	}
+
+	err := logger.Init(&opts)
+	if err != nil {
+		return err
+	}
+
+	return verify.Check(scenarioFile)
 }
