2ae91865618dcf7e1feb71c6e07939c7241ec693
Peter W. Morreale <pwmorreale@gmail.com>
2025-05-04 07:40:24 -0600

    Initial request processing.
    Initial request processing.  This is incomplete atm.
    
    Signed-off-by: Peter W. Morreale <pwmorreale@gmail.com>

---
+++ internal/rest/rest.go
@@ -0,0 +1,31 @@
+//
+//  Copyright © 2025 Peter W. Morreale. All Rights Reserved.
+//
+
+// Package rest executes REST calls
+package rest
+
+import (
+	"github.com/pwmorreale/rapid/internal/config"
+)
+
+// Rest  defines the interface for managing requests and responses
+//
+//go:generate go tool counterfeiter -o ../../test/mocks/fake_rest.go . Rest
+type Rest interface {
+	Execute(*config.Request)
+}
+
+// Context defines a scenario context.
+type Context struct {
+}
+
+// New creates a new instance.
+func New() *Context {
+	return &Context{}
+}
+
+// Execute creates and executes the request then validates the response.
+func (r *Context) Execute(_ *config.Request) {
+
+}
